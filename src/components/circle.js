193dbd2432217bc8b90ac84becae364aed5572a7
Gourav9707 <67501980+Gourav9707@users.noreply.github.com>
2020-11-02 21:32:13 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,13 @@
 
 // declare class
 
-
 // export class using module.exports
+import Shape from "./shape";
+module.exports = class Circle extends Shape {
+  constructor() {
+    super();
+  }
+  calculateArea() {
+    console.log("Area od cirle");
+  }
+};
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
 
 // export class using module.exports
+module.exports = class Shape {
+  constructor() {
+    this.color = "red";
+  }
+  drawShape() {
+    console.log("Drawing a shape");
+  }
+  calculateArea() {
+    console.log("don't know area of unknow shape");
+  }
+};
